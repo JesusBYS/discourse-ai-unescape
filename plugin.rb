4a238e0e5530ebb2d0c97aa840db351baf2d409d
Stileex <stileex@gmail.com>
2026-02-10 11:48:28 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: discourse-ai-unescape
-# about: Unescapes \u003e \u003c \u0026 in AI/bot posts so Markdown stays clean
+# about: Unescapes \u003e in bot posts so Markdown quotes stay clean
 # version: 0.1
 # authors: You
 # url: https://github.com/<YOU>/discourse-ai-unescape
@@ -8,27 +8,16 @@ enabled_site_setting :ai_unescape_enabled
 
 after_initialize do
   DiscourseEvent.on(:post_created) do |post, _opts, _user|
-    next if !SiteSetting.ai_unescape_enabled
-    next if post.blank? || post.raw.blank?
-    next if post.user.blank?
-
-    # Ne touche que les comptes bot (ex: gpt4_bot, claude_bot, etc.)
+    next unless SiteSetting.ai_unescape_enabled
+    next if post.blank? || post.raw.blank? || post.user.blank?
     next unless post.user.bot?
 
     raw = post.raw
-
-    # Discourse/JSON escaping typique
-    fixed = raw
-      .gsub('\\u003e', '>')
-      .gsub('\\u003c', '<')
-      .gsub('\\u0026', '&')
+    fixed = raw.gsub('\\u003e', '>')
 
     next if fixed == raw
 
-    post.raw = fixed
-    post.save!(validate: false)
-
-    # Rebake pour que cooked reflète le raw corrigé
+    post.update_columns(raw: fixed)
     post.rebake!
   end
 end
